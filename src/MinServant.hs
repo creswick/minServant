7949bf2ae4dbf2115eef64403e9dc448f605741e
Rogan Creswick <creswick@galois.com>
2015-11-13 18:43:40 -0500

    very basic database use is working

--- src/MinServant.hs
+++ src/MinServant.hs
@@ -1,25 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 module MinServant where
 
-import Data.Aeson
+import Data.Time.Calendar (fromGregorian)
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Either (left, EitherT)
-import Data.Time.Calendar
-import GHC.Generics
 import Network.Wai
 import Servant
 
-data User = User
-  { name :: String
-  , age :: Int
-  , email :: String
-  , registration_date :: Day
-  } deriving (Eq, Show, Generic)
-
-instance ToJSON User
+import Types
+import Db
 
 -- | The API for this web service.
 -- Note that these entries *must* line up with the entries in the
@@ -37,19 +29,19 @@ type UserAPI = "users" :> Get '[JSON] [User]
 -- same type, then the logical definitions will not match, and your
 -- app will run happily -- while generating the wrong results.
 server :: Server UserAPI
-server = return users
-    :<|> return isaac
+server = users
     :<|> return albert
+    :<|> return isaac
     :<|> notAUser
 
 isaac :: User
-isaac = User "Isaac Newton" 372 "isaac@newton.co.uk" (fromGregorian 1683 3 1)
+isaac = User 1 "Isaac Newton" 372 "isaac@newton.co.uk" "1683-3-1" -- (fromGregorian 1683 3 1)
 
 albert :: User
-albert = User "Albert Einstein" 136 "ae@mc2.org" (fromGregorian 1905 12 1)
+albert = User 2 "Albert Einstein" 136 "ae@mc2.org" "1905-12-1" -- (fromGregorian 1905 12 1)
 
-users :: [User]
-users = [isaac, albert]
+users :: EitherT ServantErr IO [User]
+users = liftIO loadUsers
 
 -- | Here's a user that does not exist, it always returns a 404, just
 -- to show how you would do that in a handler.
@@ -63,7 +55,6 @@ userNotFound = err404 { errBody = "User does not exist." }
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
-
 
 app :: Application
 app = serve userAPI server
