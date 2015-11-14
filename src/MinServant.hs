bc1bfb7a3d6047c789624ecf2f04b24bb73413ee
Rogan Creswick <creswick@galois.com>
2015-11-13 19:04:08 -0500

    split out API module, used a url capture for a more reasonable error example

--- src/MinServant.hs
+++ src/MinServant.hs
@@ -12,16 +12,9 @@ import Servant
 
 import Types
 import Db
+import API
 
--- | The API for this web service.
--- Note that these entries *must* line up with the entries in the
--- `server` definition below.
-type UserAPI = "users" :> Get '[JSON] [User]
-          :<|> "albert" :> Get '[JSON] User
-          :<|> "isaac" :> Get '[JSON] User
-          :<|> "notAUser" :> Get '[JSON] User
-
--- | The set of logical handlers that go with the API above.
+-- | The set of logical handlers that go with the API.
 --
 -- As mentioned above, these handlers must be listed in the same order
 -- as the API entries.  If they do not, then you will (hopefully) get
@@ -30,9 +23,9 @@ type UserAPI = "users" :> Get '[JSON] [User]
 -- app will run happily -- while generating the wrong results.
 server :: Server UserAPI
 server = users
+    :<|> getUser
     :<|> return albert
     :<|> return isaac
-    :<|> notAUser
 
 isaac :: User
 isaac = User 1 "Isaac Newton" 372 "isaac@newton.co.uk" "1683-3-1" -- (fromGregorian 1683 3 1)
@@ -43,11 +36,12 @@ albert = User 2 "Albert Einstein" 136 "ae@mc2.org" "1905-12-1" -- (fromGregorian
 users :: EitherT ServantErr IO [User]
 users = liftIO loadUsers
 
--- | Here's a user that does not exist, it always returns a 404, just
--- to show how you would do that in a handler.
--- notAUser :: User
-notAUser :: EitherT ServantErr IO User
-notAUser = left userNotFound
+getUser :: Int -> EitherT ServantErr IO User
+getUser theId = do
+  res <- liftIO $ loadUser theId
+  case res of
+    Left  _err -> left userNotFound
+    Right user -> return user
 
 -- | A simple "user not found" 404 error.
 userNotFound :: ServantErr
