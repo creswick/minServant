8ff812398d56aac180a5aaebef3980379fc322b2
Rogan Creswick <creswick@galois.com>
2015-12-18 16:26:48 -0800

    initial work using hssqlppp to pre-process sql queries at compile-time

--- src/Db.hs
+++ src/Db.hs
@@ -3,27 +3,50 @@
 {-# LANGUAGE QuasiQuotes         #-}
 module Db where
 
-import           Data.ByteString
-import           Database.PostgreSQL.Simple as PG
+import qualified Data.ByteString as BS
+import           Data.String (fromString)
+import qualified Database.PostgreSQL.Simple as PG
 import           Database.PostgreSQL.Simple.SqlQQ ( sql )
+import           Database.HsSqlPpp.Ast
+import           Database.HsSqlPpp.Quote
+import           Database.HsSqlPpp.Annotation
+import           Database.HsSqlPpp.Pretty
 import qualified Control.Exception as X
 
 import           Types
 
-connInfo :: ConnectInfo
-connInfo = defaultConnectInfo
-  { connectUser     = "creswick"
-  , connectPassword = ""
-  , connectDatabase = "servant"
+-- test :: Statement
+-- test = [$sqlStmt|
+--   create table $(tablename) (
+--    $(varname) $(typename)
+--   );
+--         |]
+--   where
+--     tablename = "my_table"
+--     varname = "my_field"
+--     typename = "text"
+
+query :: (PG.FromRow r) => PG.Connection -> Statement -> IO (Either X.SomeException [r])
+query conn stmt = X.try (PG.query_ conn $ fromString $ printStatements [stmt])
+
+connInfo :: PG.ConnectInfo
+connInfo = PG.defaultConnectInfo
+  { PG.connectUser     = "creswick"
+  , PG.connectPassword = ""
+  , PG.connectDatabase = "servant"
   }
 
 loadUsers :: IO [User]
-loadUsers = X.bracket (connect connInfo) PG.close $ \conn -> do
-  PG.query_ conn [sql| SELECT * FROM users |]
+loadUsers = do
+  res <- X.bracket (PG.connect connInfo) PG.close $ \conn -> do
+              query conn [$sqlStmt| SELECT * FROM users; |]
+  case res of
+    Left    err -> return []
+    Right users -> return users
 
 loadUser :: Int -> IO (Either String User)
-loadUser theId = X.bracket (connect connInfo) PG.close $ \conn -> do
-  us <- PG.query conn [sql| SELECT *
+loadUser theId = X.bracket (PG.connect connInfo) PG.close $ \conn -> do
+  us <- PG.query conn [$sql| SELECT *
                            FROM users
                            WHERE user_id=?
                           |] (PG.Only theId)
