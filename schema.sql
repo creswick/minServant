2cadea04292ec41f0ae4fb73ead3426cc7c0c76e
Rogan Creswick <creswick@galois.com>
2015-12-25 08:56:35 -0600

    added more sample data

--- schema.sql
+++ schema.sql
@@ -8,3 +8,9 @@ CREATE TABLE users (
 
 insert into users (name, age, email, registration_date) values ('Isaac Newton', 372, 'isaac@newton.co.uk', '1683-3-1');
 insert into users (name, age, email, registration_date) values ('Albert Einstein', 136, 'ae@mc2.org', '1905-12-1');
+insert into users (name, age, email, registration_date) values ('I. Newton', 200, 'isaac@newton.co.uk', '1683-3-1');
+insert into users (name, age, email, registration_date) values ('A. Einstein', 50, 'ae@mc2.org', '1905-12-1');
+insert into users (name, age, email, registration_date) values ('Newt Newt', 72, 'isaac@newton.co.uk', '1683-3-1');
+insert into users (name, age, email, registration_date) values ('Albert', 6, 'ae@mc2.org', '1905-12-1');
+insert into users (name, age, email, registration_date) values ('Isaac N.', 18, 'isaac@newton.co.uk', '1683-3-1');
+insert into users (name, age, email, registration_date) values ('A. E.', 136, 'ae@mc2.org', '1905-12-1');
